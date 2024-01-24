34521a32f72288a0e4cc4785a28c85a5f52a4036
Tuomo Ahonen <tuomo.ahonen@live.fi>
2024-01-24 20:43:47 +0200

    done, I think

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {useState} from 'react';
+import {StyleSheet, Text, TextInput, View, Button, FlatList} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [newItem, setNewItem] = useState('');
+	const [itemsList, setItemsList] = useState([]);
+
+	const handleAdd = () => {
+		if (!newItem) return;
+		setItemsList([...itemsList, newItem.slice(0, 1).toUpperCase() + newItem.slice(1).toLowerCase()]);
+		setNewItem('');
+	};
+
+	const handleClear = () => {
+		setItemsList([]);
+		setNewItem('');
+	};
+
+	//console.log(itemsList);
+
+	return (
+		<View style={styles.container}>
+			<TextInput value={newItem} onChangeText={(e) => setNewItem(e)} style={styles.textInput} />
+			<View style={styles.buttonsAreaView}>
+				<View style={styles.buttonViewLeft}>
+					<Button title='ADD' onPress={handleAdd} />
+				</View>
+				<View style={styles.buttonViewRight}>
+					<Button title='CLEAR' onPress={handleClear} />
+				</View>
+			</View>
+			<Text style={styles.shoppingList}>My shoppinglist</Text>
+			<FlatList data={itemsList} renderItem={({index, item}) => <Text key={index}>{item}</Text>} />
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		marginTop: StatusBar.currentHeight ? StatusBar.currentHeight + 50 : 50,
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	textInput: {
+		width: 200,
+		margin: 5,
+		paddingLeft: 5,
+		paddingRight: 5,
+		borderWidth: 1,
+		borderColor: '#000000',
+	},
+	buttonsAreaView: {
+		margin: 5,
+		flexDirection: 'row',
+	},
+	buttonViewLeft: {
+		marginRight: 5,
+	},
+	buttonViewRight: {
+		// nothing here yet
+	},
+	shoppingList: {
+		margin: 5,
+		fontSize: 20,
+	},
 });
+
